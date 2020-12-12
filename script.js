130f1a18ed7d703ac974bdf3d304218eb724176e
Isaiah-c-alt <73313464+Isaiah-c-alt@users.noreply.github.com>
2020-12-11 22:43:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-alert("The only css used for this page is in the navigation bar. The rest of this page is made with HTML attributes");
+setTimeout(function(){ alert("The only css used for this page is in the navigation bar. The rest of this page is made with HTML attributes"); }, 1000);
